b9c971c78fffbfe4770f2c822e17aabb9d5f162b
Gaurav <gaurav@Gauravs-MacBook-Air.local>
2025-01-10 20:31:08 +0530

    exit and help commands

--- repl.go
+++ repl.go
@@ -7,7 +7,29 @@ import (
 	"strings"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func GetCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Display a help message",
+			callback:    commandHelp,
+		},
+	}
+}
+
 func StartRepl() {
+
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
 		fmt.Print("Pokedex > ")
@@ -16,11 +38,23 @@ func StartRepl() {
 			fmt.Fprintln(os.Stderr, "reading standard input:", err)
 			return
 		}
-		userInput := scanner.Text()
-		cleanUserInputArr := CleanInput(userInput)
-		if len(cleanUserInputArr) > 0 {
-			fmt.Printf("Your command was: %v\n", cleanUserInputArr[0])
+
+		cleanUserInputArr := CleanInput(scanner.Text())
+		if len(cleanUserInputArr) == 0 {
+			continue
 		}
+
+		command := cleanUserInputArr[0]
+		if cmd, ok := GetCommands()[command]; ok {
+			err := cmd.callback()
+			if err != nil {
+				fmt.Printf("Error : %v\n", err)
+			}
+		} else {
+			fmt.Println("Unknown command")
+			continue
+		}
+
 	}
 }
 
