be9c6cfbe82683de145ed674f5729cb3398d306c
Gaurav <gaurav@Gauravs-MacBook-Air.local>
2025-01-10 19:54:31 +0530

    basic repl

--- repl.go
+++ repl.go
@@ -1,8 +1,28 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
+
+func StartRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		if err := scanner.Err(); err != nil {
+			fmt.Fprintln(os.Stderr, "reading standard input:", err)
+			return
+		}
+		userInput := scanner.Text()
+		cleanUserInputArr := CleanInput(userInput)
+		if len(cleanUserInputArr) > 0 {
+			fmt.Printf("Your command was: %v\n", cleanUserInputArr[0])
+		}
+	}
+}
 
 func CleanInput(text string) []string {
 	output := strings.ToLower(text)
